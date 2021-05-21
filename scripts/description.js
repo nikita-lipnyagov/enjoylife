5d5c613d2b27f997d51fbda3979477319398a7e8
nikita-lipnyagov <nikitalipnagov@gmail.com>
2021-05-21 15:36:26 +0300

    Refactored project

--- scripts/description.js
+++ scripts/description.js
@@ -1,69 +1,78 @@
 let isPlaying = false;
-      let backgroundSoundMode = window.localStorage.backgroundSoundMode;
+let intervalId;
 
-      $('#backgroundAudioPanel')[0].volume = 0.5;
+$('#backgroundAudioPanel')[0].volume = 0.2;
 
-      function playAudio() {
-        if (!isPlaying) {
-          if (backgroundSoundMode === 'true') {
-            $('#backgroundAudioPanel')[0].play();
-          }
-          
-          $('#practiceAudioPanel')[0].play();
-          $('#playButton').html('<i class="icon-control-pause float-right" style="width: 24px; height: 24px;"></i>');
-          isPlaying = true;
-        } else {
-          if (backgroundSoundMode === 'true') {
-            $('#backgroundAudioPanel')[0].pause();
-          }
+function playAudio() {
+  const backgroundAudioPanel = $('#backgroundAudioPanel')[0];
+  const practiceAudioPanel = $('#practiceAudioPanel')[0];
 
-          $('#practiceAudioPanel')[0].pause();
-          $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
-          isPlaying = false;
-         }
-      }
+  if (!isPlaying) {
+      backgroundAudioPanel.play();
+      practiceAudioPanel.play();
+      $('#playButton').html('<i class="icon-control-pause float-right" style="width: 24px; height: 24px;"></i>');
 
-      function hideAudioEndInfo() {
-        $('#audio-end-info').css('display', 'none');
-      }
+      intervalId = window.setInterval(function() {
+        $('#minutesToPracticeEnd').html(Math.ceil((practiceAudioPanel.duration - practiceAudioPanel.currentTime)/60));
+      }, 3000);
 
-      $('#practiceAudioPanel').on('ended', function() {
-        $('#practiceAudioPanel')[0].pause();
-        $('#practiceAudioPanel')[0].currentTime = 0;
+      isPlaying = true;
+    } else {
+      backgroundAudioPanel.pause();
+      practiceAudioPanel.pause();
+      $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
+
+      clearInterval(intervalId);
+
+      isPlaying = false;
+    }
+}
+
+function hideAudioEndInfo() {
+  $('#audio-end-info').css('display', 'none');
+
+  $('#audioModalLabel').html(`Practice will end in <span id="minutesToPracticeEnd">${Math.ceil((practiceAudioPanel.duration - practiceAudioPanel.currentTime)/60)}</span> mins`);
+}
+
+$('#practiceAudioPanel').on('ended', function() {
+  $('#backgroundAudioPanel')[0].pause();
+  $('#backgroundAudioPanel')[0].currentTime = 0;
+
+  $('#practiceAudioPanel')[0].pause();
+  $('#practiceAudioPanel')[0].currentTime = 0;
         
-        $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
-        $('#audio-end-info').css('display', 'block');
+  $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
+  $('#audio-end-info').css('display', 'block');
 
-        if (backgroundSoundMode === 'true') {
-            $('#backgroundAudioPanel')[0].pause();
-            $('#backgroundAudioPanel')[0].currentTime = 0;
-        }
+  clearInterval(intervalId);
 
-        isPlaying = false;
-      });
+  $('#audioModalLabel').html('Practice ended');
 
-      $('#replayButton').hover(function(){
-            $('#replaySvg').css('fill', 'var(--text-main-color)');
-        }, function(){
-            $('#replaySvg').css('fill', 'var(--main-bg-color)');
-      });
+  isPlaying = false;
+});
 
-      $('#forwardButton').hover(function(){
-            $('#forwardSvg').css('fill', 'var(--text-main-color)');
-        }, function(){
-            $('#forwardSvg').css('fill', 'var(--main-bg-color)');
-      });
+$('#replayButton').hover(function(){
+    $('#replaySvg').css('fill', 'var(--text-main-color)');
+  }, function(){
+    $('#replaySvg').css('fill', 'var(--main-bg-color)');
+});
 
-      $("#audioModal").on("hidden.bs.modal", function () {
-        $('#practiceAudioPanel')[0].pause();
-        $('#practiceAudioPanel')[0].currentTime = 0;
+$('#forwardButton').hover(function(){
+    $('#forwardSvg').css('fill', 'var(--text-main-color)');
+  }, function(){
+    $('#forwardSvg').css('fill', 'var(--main-bg-color)');
+});
+
+$("#audioModal").on("hidden.bs.modal", function () {
+  $('#backgroundAudioPanel')[0].pause();
+  $('#backgroundAudioPanel')[0].currentTime = 0;
+
+  $('#practiceAudioPanel')[0].pause();
+  $('#practiceAudioPanel')[0].currentTime = 0;
         
-        $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
+  $('#playButton').html('<i class="icon-control-play float-right" style="width: 24px; height: 24px;"></i>');
 
-        if (backgroundSoundMode === 'true') {
-            $('#backgroundAudioPanel')[0].pause();
-            $('#backgroundAudioPanel')[0].currentTime = 0;
-        }
+  clearInterval(intervalId);
 
-        isPlaying = false;
-      });
+  isPlaying = false;
+});
