6140a2b136355ba2e4a2bde7098f6dd6c0f089c9
nikita-lipnyagov <nikitalipnagov@gmail.com>
2021-05-31 14:47:53 +0300

    Updated test event

--- serviceWorker.js
+++ serviceWorker.js
@@ -65,6 +65,4 @@ self.addEventListener("fetch", (fetchEvent) => {
 self.addEventListener('message', event => {
     // event is an ExtendableMessageEvent object
     console.log(`The client sent me a message: ${event.data}`);
-  
-    event.source.postMessage("Hi client");
 });
